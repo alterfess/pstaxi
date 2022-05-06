da511918ad2c4ede4c0f733c527e661b68d59d14
George Sidorov <georgesidorov93@gmail.com>
2022-05-06 07:10:54 +0500

    New class

--- motorpool/models.py
+++ motorpool/models.py
@@ -2,11 +2,36 @@ from django.db import models
 
 
 class Brand(models.Model):
-    title = models.CharField(max_length=104, verbose_name='Название')
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name_plural = 'Бренды'
         verbose_name = 'Бренд'
 
+
+class Auto(models.Model):
+    AUTO_CLASS_ECONOMY = 'e'
+    AUTO_CLASS_COMFORT = 'c'
+    AUTO_CLASS_BUSINESS = 'b'
+
+    AUTO_CLASS_CHOICES = (
+        (AUTO_CLASS_ECONOMY, 'economy'),
+        (AUTO_CLASS_COMFORT, 'comfort'),
+        (AUTO_CLASS_BUSINESS, 'business'),
+    )
+    number = models.CharField(max_length=15)
+    description = models.TextField(max_length=500, default='', blank=True)
+    year = models.PositiveSmallIntegerField(null=True)
+    auto_class = models.CharField(max_length=1, null=True, choices=AUTO_CLASS_CHOICES, default=AUTO_CLASS_ECONOMY)
+
     def __str__(self):
-        return self.title
+        return self.number
+
+    class Meta:
+        verbose_name_plural = 'Автомобили'
+        verbose_name = 'Автомобиль'
+
+
